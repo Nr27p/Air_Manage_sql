bfb25dedcd003723e3721c522b76970a21f59cb6
Girish Nhavkar <girishnhavkar44@gmail.com>
2024-07-20 11:44:31 +0530

    done

--- frontend/airport-management-system/src/app/page.tsx
+++ frontend/airport-management-system/src/app/page.tsx
@@ -1,23 +1,4 @@
-// import React from 'react';
 
-// // import FlightList from '../components/FlightList';
-// import FlightForm from '../../components/FlightsForm';
-// import FlightList from '../../components/FlightList';
-// import Navbar from '../../components/Navbar';
-
-// const HomePage: React.FC = () => {
-//   return (
-//     <div>
-//       <h1>Flight Management</h1>
-//       <Navbar />
-//       <FlightForm />
-//       <FlightList />
-      
-//     </div>
-//   );
-// };
-
-// export default HomePage;
 
 'use client';
 
